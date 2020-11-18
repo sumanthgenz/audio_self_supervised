551c159944fa00ade1b5f1a17ca052cf58a58e5a
Sumanth Gurram <sumanthgenz@gmail.com>
2020-11-18 14:03:47 -0800

    byol and trainer skeleton done

--- encoder.py
+++ encoder.py
@@ -36,197 +36,106 @@ import gc
 import torchaudio
 from audio_self_supervised.dataloader import AudioDataset
 
+#Implementation from
+# https://github.com/CVxTz/COLA_pytorch/blob/master/audio_encoder/encoder.py
+class FeatureModel(torch.nn.Module):
+    def __init__(self, drop_connect_rate=0.1):
+        super(FeatureModel, self).__init__()
 
-class Encoder(pl.LightningModule):
+        self.cnn1 = torch.nn.Conv2d(1, 3, kernel_size=3)
+        self.efficientnet = EfficientNet.from_name(
+            "efficientnet-b0", include_top=False, drop_connect_rate=drop_connect_rate
+        )
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+
+        x = self.cnn1(x)
+        x = self.efficientnet(x)
+
+        y = x.squeeze(3).squeeze(2)
+        return y
+
+class Encoder(torch.nn.Module):
 
     def __init__(self):
-        super(Net, self).__init__()
-        self.num_features = 13
-        self.input_size_half = 256
-        self.input_size = 512
-        self.seq_len = 500
-        self.hidden_size_1 = 1024
-        self.hidden_size_2 = 2048
-        self.hidden_size_3 = 1024
-        self.num_classes = 700
-        self.num_epochs = 100
-        self.batch_size = 16
-        self.learning_rate = 0.00025
-        self.attention_heads = 8
-        self.n_layers = 16
-        self.counter = 0
+        super(Encoder, self).__init__()
 
-        self.trainPath = 'train'
-        self.testPath = 'test'
-        self.valPath = 'validate'
-        self.classes = self.get_pickle("classes_dict")
+        self._model_dimension = 512
 
-        
-        
-        self.encode = nn.TransformerEncoderLayer(d_model=self.input_size, nhead=self.attention_heads, dropout=0.15)
-        self.transformer = nn.TransformerEncoder(self.encode, num_layers=self.n_layers)
+        self._fc1 = nn.Linear(1280, 512)
+        self._fc2 = nn.Linear(512, 512, bias=False)
 
-        self.fc0 = nn.Linear(self.num_features, self.input_size_half)
-        self.fc1 = nn.Linear(self.input_size_half, self.input_size)
-        self.fc2 = nn.Linear(self.input_size, self.hidden_size_1) 
-        self.fc3 = nn.Linear(self.hidden_size_1, self.hidden_size_2)
-        self.fc4 = nn.Linear(self.hidden_size_2, self.hidden_size_3)
-        self.fc5 = nn.Linear(self.hidden_size_3, self.num_classes)
+        self._norm1 = nn.LayerNorm(normalized_shape=512)
+      
+        self._dropout = nn.Dropout(p=0.15)
+        self._tanh = torch.tanh()
 
-        self.old_fc4 = nn.Linear(self.hidden_size_1, self.num_classes)
+        self._loss = nn.CrossEntropyLoss()
+
+        self._encoder = FeatureModel(drop_connect_rate=0.1)
 
 
-        self.norm1 = nn.LayerNorm(self.input_size_half)
-        self.norm2 = nn.LayerNorm(self.input_size)
-        self.norm3 = nn.LayerNorm(self.hidden_size_1)
-        self.norm4 = nn.LayerNorm(self.hidden_size_2)
-        self.norm5 = nn.LayerNorm(self.hidden_size_3)
+        #Implementation from
+        #https://github.com/CannyLab/aai/blob/ddc76404bdfe15fb8218c31d9dc6859f3d5420db/aai/research/gptcaptions/models/encoders/predictive_byol.py
+        self._representation_mlp = torch.nn.Sequential(
+            torch.nn.Linear(self._model_dimension, self._model_dimension),
+            torch.nn.BatchNorm1d(self._model_dimension),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self._model_dimension, self._model_dimension),
+        )
+        self._byol_predictor = torch.nn.Sequential(
+            torch.nn.Linear(self._model_dimension, self._model_dimension),
+            torch.nn.BatchNorm1d(self._model_dimension),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self._model_dimension, self._model_dimension),
+        )
+
+        self._translation_mlp = torch.nn.Sequential(
+            torch.nn.Linear(2 * self._model_dimension, self._model_dimension),
+            torch.nn.ReLU(),
+            torch.nn.Linear(self._model_dimension, self._model_dimension),
+        )
+
+        self._target_encoder = None
+        self._target_networks_initialized = False
+        self._ema_beta = ema_beta
 
 
-        self.dropout = nn.Dropout(p=0.15)
-        self.relu = nn.ReLU()
+    def get_paramaters(self,):
+        params = []
+        params += list(self._encoder.parameters())
+        if self._target_encoder is not None:
+            params += list(self._target_encoder.parameters())
+        params += list(self._representation_mlp.parameters())
+        params += list(self._byol_predictor.parameters())
+        params += list(self._translation_mlp.parameters())
 
-        self.loss = nn.CrossEntropyLoss()
+        return params
 
-        self.train_test_split_pct = 0.8
-        self.train_dataset, self.test_dataset, self.val_dataset = self.prepare_data()
-        self.conf_target = ()
-        self.conf_pred = ()
-        self.hist_count = 0
+    def reset_moving_average(self):
+        del self._target_encoder
+        self._target_encoder = None
 
-    def get_pickle(self, classPath):
-            with open('Desktop/kinetics_{}.pickle'.format(classPath), 'rb') as handle:
-                result = pickle.load(handle)
-            return result
+    def _ema_copy_model(self, online_model, target_model):
+        for current_params, target_params in zip(online_model.parameters(), target_model.parameters()):
+            old_weight, new_weight = target_params.data, current_params.data
+            target_params.data = old_weight * self._ema_beta + (1 - self._ema_beta) * new_weight
 
-    def create_class_hists(self, data, name):
-        hist_list = []
-        for i in data.indices:
-            hist_list.append(data.dataset[i][1])
-        labels, values = zip(*Counter(hist_list).items())
-        indexes = np.arange(len(labels))
-        width = 1
-        plt.bar(indexes, values, width)
-        plt.xticks(indexes + width * 0.5, labels)
-        plt.savefig(name)
-        
+    def update_moving_average(self):
+        if self._target_encoder is not None:
+            self._ema_copy_model(self._encoder, self._target_encoder)
     
-    def forward(self, x, mask):
-        expand = self.fc0(x)
-        expand = self.norm1(self.dropout(expand))
-        expand2 = self.fc1(expand)
-        src = self.norm2(self.dropout(expand2))
+    def forward(self, x):
+        x1, x2 = x
 
-        transform = self.transformer(src, src_key_padding_mask=mask)
+        x1 = self._dropout(self._feature_model(x1))
+        x1 = self._dropout(self.fc1(x1))
+        x1 = self._dropout(self.tanh(self.norm1(x1)))
 
-        transform = self.relu(self.fc2(transform.permute(1, 0, 2).mean(dim=1)))
-        transform = transform + self.dropout(transform)
-        transform = self.norm3(transform)
-
-        out = self.fc4(self.dropout(self.relu(self.fc3(transform))))
-        out = transform + self.dropout(out)
-        out = self.norm3(out)
-        out = self.fc5(out)
+        x2 = self._dropout(self._feature_model(x2))
+        x2 = self._dropout(self.fc1(x2))
+        x2 = self._dropout(self.tanh(self.norm1(x2)))
         
-        return out
-
-    def prepare_data(self):
-        train_dataset = AudioDataset(self.trainPath)
-        test_dataset = AudioDataset(self.testPath)
-        val_dataset = AudioDataset(self.valPath)
-        return train_dataset, test_dataset, val_dataset
+        return x1, x2
     
-    def collate_fn(self, batch):
-        # return torch.utils.data.dataloader.default_collate(batch)
-
-        def trp(arr, n):
-            arr = arr.tolist()
-            seq = []
-            for cmp in arr:
-                new_cmp = cmp[:n] + [0.0]*(n-len(cmp))
-                seq.append(new_cmp)
-            result = np.transpose(np.array(seq)).tolist()
-            return result
-
-        def pad_masker(arr):
-            zer = torch.zeros(self.num_features)
-            ind = 0
-            for t in arr:
-                pad = torch.all(torch.eq(t, zer))
-                if  pad == 1:
-                    break
-                ind += 1
-            result = ([True]*ind) + ([False]*(self.seq_len - ind))
-            return result
-        
-        batch = np.transpose(batch)
-        data = list(filter(lambda x: x is not None, batch[0]))
-        labels = list(filter(lambda x: x is not None, batch[1]))
-
-        samples = torch.Tensor([(trp(t, self.seq_len))for t in data])
-        mask = torch.Tensor([pad_masker(t) for t in samples])
-        labels = torch.Tensor(labels).long()
-        samples = samples.permute(1, 0, 2)
-
-        return samples, labels, mask
-
-    def train_dataloader(self):
-        self.train_loader = torch.utils.data.DataLoader(dataset=self.train_dataset, 
-                                                        batch_size=self.batch_size, 
-                                                        shuffle=False,
-                                                        collate_fn=self.collate_fn,
-                                                        num_workers=16)
-        return self.train_loader
-
-
-    def val_dataloader(self):
-        self.test_loader = torch.utils.data.DataLoader(dataset=self.val_dataset, 
-                                                        batch_size=self.batch_size, 
-                                                        shuffle=False,
-                                                        collate_fn=self.collate_fn,
-                                                        num_workers=16)        
-        return self.test_loader
-
-    def training_step(self, batch, batch_idx):
-        data, target, mask = batch
-        mask = mask < 0
-        output = self.forward(data, mask)
-
-        loss = self.loss(output, target)
-        logs = {'loss': loss}
-        return {'loss': loss, 'log': logs}
-
-    def validation_step(self, batch, batch_idx):
-        data, target, mask = batch
-        mask = mask < 0
-        output = self.forward(data, mask)
-        temp_loss = self.loss(output, target)
-        pred = output.argmax(dim=1, keepdim=True)
-
-        correct = pred.eq(target.view_as(pred)).sum().item()
-        acc = torch.tensor(correct/self.batch_size)
-        logs = {'val_loss': temp_loss, 'val_acc': acc}
-
-        # if self.counter >= self.num_epochs-1:
-            # wandb.sklearn.plot_confusion_matrix(target.cpu().numpy(), pred.flatten().cpu().numpy(), self.classes)
-
-        return {'val_loss': temp_loss, 'val_acc': acc}
-    
-    def validation_epoch_end(self, outputs):
-        # print(outputs)
-        self.counter += 1
-        self.hist_count += 1
-        avg_loss = torch.stack([m['val_loss'] for m in outputs]).mean()
-        avg_acc = torch.stack([m['val_acc'] for m in outputs]).mean()
-        logs = {'val_loss': avg_loss, 'val_acc': avg_acc}
-        return {'val_loss': avg_loss, 'log': logs}
-    
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-        # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001, steps_per_epoch=29, epochs=self.num_epochs)
-        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.02)
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
-        # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[70,80, 85, 90, 95], gamma=0.1)
-        # return [optimizer], [scheduler]
-        return optimizer
--- train.py
+++ train.py
@@ -44,6 +44,75 @@ np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 # torch.cuda.empty_cache()
 
 
+class ModalBYOL(pl.LightningModule):
+
+    def __init__(self,):
+        super().__init__()
+        self.model = None
+
+
+    def train_dataloader(self):
+        self.train_loader = torch.utils.data.DataLoader(dataset=self.train_dataset, 
+                                                        batch_size=self.batch_size, 
+                                                        shuffle=False,
+                                                        collate_fn=self.collate_fn,
+                                                        num_workers=16)
+        return self.train_loader
+
+
+    def val_dataloader(self):
+        self.test_loader = torch.utils.data.DataLoader(dataset=self.val_dataset, 
+                                                        batch_size=self.batch_size, 
+                                                        shuffle=False,
+                                                        collate_fn=self.collate_fn,
+                                                        num_workers=16)        
+        return self.test_loader
+
+    def training_step(self, batch, batch_idx):
+        data, target, mask = batch
+        mask = mask < 0
+        output = self.forward(data, mask)
+
+        loss = self.loss(output, target)
+        logs = {'loss': loss}
+        return {'loss': loss, 'log': logs}
+
+    def validation_step(self, batch, batch_idx):
+        data, target, mask = batch
+        mask = mask < 0
+        output = self.forward(data, mask)
+        temp_loss = self.loss(output, target)
+        pred = output.argmax(dim=1, keepdim=True)
+
+        correct = pred.eq(target.view_as(pred)).sum().item()
+        acc = torch.tensor(correct/self.batch_size)
+        logs = {'val_loss': temp_loss, 'val_acc': acc}
+
+        # if self.counter >= self.num_epochs-1:
+            # wandb.sklearn.plot_confusion_matrix(target.cpu().numpy(), pred.flatten().cpu().numpy(), self.classes)
+
+        return {'val_loss': temp_loss, 'val_acc': acc}
+    
+    def validation_epoch_end(self, outputs):
+        # print(outputs)
+        self.counter += 1
+        self.hist_count += 1
+        avg_loss = torch.stack([m['val_loss'] for m in outputs]).mean()
+        avg_acc = torch.stack([m['val_acc'] for m in outputs]).mean()
+        logs = {'val_loss': avg_loss, 'val_acc': avg_acc}
+        return {'val_loss': avg_loss, 'log': logs}
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+        # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001, steps_per_epoch=29, epochs=self.num_epochs)
+        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.02)
+        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+        # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[70,80, 85, 90, 95], gamma=0.1)
+        # return [optimizer], [scheduler]
+        return optimizer
+
+
 if __name__ == '__main__':
     
 
